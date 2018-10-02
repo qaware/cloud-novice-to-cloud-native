90d705cda9502a76c45e5f0b6eead4b277128267
Sonja Wittke <sonja.wittke@qaware.de>
2018-10-02 10:46:37 +0200

    Returning json instead of html for metrics and host information. Added dependency to ktor-jackson for json processing and moved dependency version definitions to gradle build file.

--- src/Application.kt
+++ src/Application.kt
@@ -1,17 +1,27 @@
 package de.qaware.ada.lovelace
 
 import io.ktor.application.Application
-
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.html.*
-import io.ktor.routing.*
-import kotlinx.html.*
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.html.respondHtml
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import kotlinx.html.body
+import kotlinx.html.p
 import java.net.InetAddress
 
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+        }
+    }
 
     routing {
         get("/hi") {
@@ -30,26 +40,22 @@ fun Application.main() {
             }
         }
         get("/host") {
-            call.respondHtml {
-                body {
-                    val ip = InetAddress.getLocalHost()
-                    val hostname = ip.hostName
-                    p { +"I am running on host: $hostname" }
-                }
-            }
+            val ip = InetAddress.getLocalHost()
+            call.respond(
+                    mapOf(
+                            "Host name" to ip.hostName,
+                            "Host address" to ip.hostAddress)
+            )
         }
         get("/metrics") {
-            call.respondHtml {
-                body {
-                    val runtime = Runtime.getRuntime()
-                    p { +"Just giving you some inside information on my runtime:" }
-                    p { +"Available Processors: ${runtime.availableProcessors()}" }
-                    p { +"Free Memory: ${runtime.freeMemory()}" }
-                    p { +"Total Memory: ${runtime.totalMemory()}" }
-                    p { +"Max Memory: ${runtime.maxMemory()}" }
-                    p { +"Internally used username: ${System.getProperty("user.name")}" }
-                }
-            }
+            val runtime = Runtime.getRuntime()
+            call.respond(
+                    mapOf(
+                            "Max Memory" to runtime.totalMemory(),
+                            "Free Memory" to runtime.freeMemory(),
+                            "Total Memory" to runtime.totalMemory(),
+                            "Available Processors" to runtime.availableProcessors(),
+                            "Internally used username" to System.getProperty("user.name")))
         }
     }
 }
